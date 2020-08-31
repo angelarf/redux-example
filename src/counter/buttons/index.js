563c03da4b7b362d6749d74e60f2a123f153ee97
Angela Rodrigues Ferreira <arferreira@uolinc.com>
2020-08-30 21:19:45 -0300

    Add mais funcionalidades para os botoes. (apenas visual)

--- src/counter/buttons/index.js
+++ src/counter/buttons/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import "./style.css";
 
-const CounterButtons = ({ increment, decrement }) => {
+const CounterButtons = ({ increment, decrement, incrementCustom }) => {
+  const [customValue, setCustomValue] = useState(2);
   const handleDecrement = () => {};
   const handleIncrement = () => {};
+  const handleIncrementCustom = () => {};
+  const handleIncrementAsync = () => {};
 
   return (
     <div className="buttons-wrapper">
@@ -13,6 +16,28 @@ const CounterButtons = ({ increment, decrement }) => {
       <button className="counter-button" onClick={handleIncrement}>
         + 1
       </button>
+      <div className="counter-custom-wrapper">
+        <input
+          type="number"
+          className="counter-input"
+          value={customValue}
+          onChange={({ target }) => setCustomValue(target.value)}
+        />
+        <button
+          className="counter-button counter-button--custom"
+          onClick={handleIncrementCustom}
+        >
+          Somar
+        </button>
+      </div>
+      <div className="counter-custom-wrapper">
+        <button
+          className="counter-button counter-button--custom"
+          onClick={handleIncrementAsync}
+        >
+          + 1 Async
+        </button>
+      </div>
     </div>
   );
 };
